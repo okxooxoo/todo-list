28f840e610bad8cc26c64fdea787c88a9ed288ec
김서로 <okxooxoo@naver.com>
2025-07-23 18:39:52 +0900

    feat: 투두리스트 구현

--- App.js
+++ App.js
@@ -1,17 +1,130 @@
+import TodoStats from './components/TodoStats.js'
 import TodoList from './components/TodoList.js'
 import { model } from './model/model.js'
 
 function App() {
   this.data = []
+  this.todoStats = null
   this.todoList = null
+  this.$todoStats = document.querySelector('#todo-stats')
   this.$todoList = document.querySelector('#todo-list')
 
   this.init = () => {
-    this.data = model
-    this.todoList = new TodoList(this.$todoList, this.data)
+    const savedData = localStorage.getItem('todos')
+    this.data = savedData ? JSON.parse(savedData) : model
+
+    this.data = this.data.map(todo => ({ ...todo, isEditing: false }))
+
+    this.todoStats = new TodoStats(this.$todoStats)
+    this.todoList = new TodoList(this.$todoList)
+
+    this.setEventListeners()
+    this.render()
   }
 
-  this.setState = () => {}
+  this.setEventListeners = () => {
+    this.$todoList.addEventListener('click', (e) => {
+      if (e.target.classList.contains('add-btn')) {
+        const $input = this.$todoList.querySelector('.new-todo-input')
+        const value = $input.value.trim()
+        if (value) {
+          $input.value = ''
+          const newTodo = { name: value, isCompleted: false, isEditing: false }
+          const updatedData = [...this.data, newTodo]
+          this.setState(updatedData)
+        }
+      }
+
+      // 전체 완료 버튼 클릭
+      if (e.target.classList.contains('complete-all-btn')) {
+        const updatedData = this.data.map(todo => ({
+          ...todo,
+          isCompleted: true
+        }))
+        this.setState(updatedData)
+      }
+
+      // 전체 삭제 버튼 클릭
+      if (e.target.classList.contains('delete-all-btn')) {
+        this.setState([])
+      }
+
+      const $li = e.target.closest('li')
+      if (!$li) return
+      const index = $li.dataset.index
+
+      if (e.target.classList.contains('toggle-complete')) {
+        const updatedData = this.data.map((todo, i) =>
+          i === Number(index)
+            ? { ...todo, isCompleted: e.target.checked }
+            : todo
+        )
+        this.setState(updatedData)
+      }
+
+      if (e.target.classList.contains('toggle-complete-text')) {
+        const updatedData = this.data.map((todo, i) =>
+          i === Number(index)
+            ? { ...todo, isCompleted: !todo.isCompleted }
+            : todo
+        )
+        this.setState(updatedData)
+      }
+
+      if (e.target.classList.contains('delete-btn')) {
+        const updatedData = this.data.filter((_, i) => i !== Number(index))
+        this.setState(updatedData)
+      }
+
+      if (e.target.classList.contains('edit-btn')) {
+        this.data[index].isEditing = true
+        this.render()
+      }
+
+      if (e.target.classList.contains('save-btn')) {
+        const $input = $li.querySelector('.edit-input')
+        const updatedData = [...this.data]
+        updatedData[index] = {
+          ...updatedData[index],
+          name: $input.value,
+          isEditing: false
+        }
+        this.setState(updatedData)
+      }
+    })
+
+    // Enter 키로도 Todo 추가할 수 있도록 함
+    this.$todoList.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        const $input = this.$todoList.querySelector('.new-todo-input')
+        const value = $input.value.trim()
+        if (value) {
+          $input.value = ''
+          this.setState([...this.data, { name: value, isCompleted: false, isEditing: false }])
+        }
+      }
+    })
+  }
+
+  this.saveToLocalStorage = () => {
+    const cleanData = this.data.map(({ name, isCompleted }) => ({
+      name,
+      isCompleted
+    }))
+    localStorage.setItem('todos', JSON.stringify(cleanData))
+  }
+
+  // 상태 변경 시 Local Storage에 저장 후 리렌더링
+  this.setState = (nextState) => {
+    this.data = nextState
+    this.saveToLocalStorage()
+    this.render()
+  }
+
+  this.render = () => {
+    this.todoList.render(this.data)
+    this.todoStats.render(this.data)
+  }
 
   this.init()
 }
--- components/TodoList.js
+++ components/TodoList.js
@@ -1,14 +1,54 @@
 function TodoList($container, data) {
   this.$container = $container
-  this.data = data
 
-  this.render = function () {
-    this.$container.innerHTML = `<ul>${this.data
-      .map(todo => `<li>${todo.name}<button type="button">X</button></li>`)
-      .join('')}</ul>`
+  this.render = (todos) => {
+    this.data = todos
+    this.$container.innerHTML = `
+    <div class="todo-input-wrapper">
+      <input type="text" class="new-todo-input" placeholder="할 일을 입력하세요" />
+      <button type="button" class="add-btn">추가</button>
+    </div>
+
+    <div class="bulk-actions">
+      <button type="button" class="complete-all-btn">전체 완료</button>
+      <button type="button" class="delete-all-btn">전체 삭제</button>
+    </div>
+
+    <ul>${this.data
+        .map(
+          (todo, index) => {
+            if (todo.isEditing) {
+              return `
+                <li data-index="${index}">
+                  <input type="text" value="${todo.name}" class="edit-input" />
+                  <button type="button" class="save-btn">저장</button>
+                </li>
+              `
+            }
+
+            if (todo.isCompleted) {
+              return `
+                <li data-index="${index}">
+                  <input type="checkbox" class="toggle-complete" checked />
+                  <span class="toggle-complete-text" style="text-decoration: line-through;">${todo.name}</span>
+                  <button type="button" class="delete-btn">삭제</button>
+                </li>
+              `
+            }
+
+            return `
+              <li data-index="${index}">
+                <input type="checkbox" class="toggle-complete" />
+                <span class="toggle-complete-text">${todo.name}</span>
+                <button type="button" class="edit-btn">수정</button>
+                <button type="button" class="delete-btn">삭제</button>
+              </li>
+            `
+          })
+        .join('')}
+    </ul>`
   }
 
-  this.render()
 }
 
 export default TodoList
